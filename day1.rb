35183501cec6fc2a3e1366523a30769670425493
JT Archie <jtarchie@gmail.com>
2024-12-02 20:06:05 -0700

    format

--- day1.rb
+++ day1.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 TEST = <<~TESTCASE
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3
+  3   4
+  4   3
+  2   5
+  1   3
+  3   9
+  3   3
 TESTCASE
 
 def part1(contents)
   lines = contents.split("\n")
   pairs = lines.map { |line| line.split(/\s+/).map(&:to_i) }.transpose
-  pairs.map(&:sort!).first.each_with_index.reduce(0) do |sum, (num, i)|
+  pairs.map(&:sort!).first.each_with_index.reduce(0) do |sum, (_num, i)|
     sum + (pairs[0][i] - pairs[1][i]).abs
   end
 end
@@ -25,7 +27,9 @@ def part2(contents)
 end
 
 raise "Test failed! 11 = #{part1(TEST)}" unless part1(TEST) == 11
+
 puts part1(File.read('day1.txt'))
 
 raise "Test failed! 31 = #{part1(TEST)}" unless part2(TEST) == 31
+
 puts part2(File.read('day1.txt'))
--- day2.rb
+++ day2.rb
@@ -1,28 +1,26 @@
+# frozen_string_literal: true
+
 TEST = <<~TESTCASE
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9
 TESTCASE
 
 def part1(contents)
   lines = contents.split("\n")
   parts = lines.map { |line| line.split(/\s+/).map(&:to_i) }
-  calculate(parts)
-end
-
-def valid?(part)
-  deltas = (0..part.length-2).map { |i| part[i] - part[i+1] }
-  deltas.all? { |delta| deltas[0].positive? == delta.positive? || deltas[0].negative? == delta.negative? } &&
-  deltas.all? { |delta| (1..3).include?(delta.abs) }
-end
-
-def calculate(parts)
   parts.filter do |part|
     valid?(part)
   end.count
+end
+
+def valid?(part)
+  deltas = (0..part.length - 2).map { |i| part[i] - part[i + 1] }
+  deltas.all? { |delta| deltas[0].positive? == delta.positive? || deltas[0].negative? == delta.negative? } &&
+    deltas.all? { |delta| (1..3).include?(delta.abs) }
 end
 
 def part2(contents)
@@ -34,15 +32,16 @@ def part2(contents)
       true
     else
       part.each_with_index.any? do |_, i|
-        valid?(part[0...i] + part[i+1..-1])
+        valid?(part[0...i] + part[i + 1..])
       end
     end
   end.tally[true]
-
 end
 
 raise "Test failed! 2 = #{part1(TEST)}" unless part1(TEST) == 2
+
 puts part1(File.read('day2.txt'))
 
 raise "Test failed! 4 = #{part2(TEST)}" unless part2(TEST) == 4
+
 puts part2(File.read('day2.txt'))

4e8f8307107d54ca2356e54a4f82c1b294b5f3c2
JT Archie <jtarchie@gmail.com>
2024-12-02 19:28:29 -0700

    init

---
+++ day1.rb
@@ -0,0 +1,31 @@
+TEST = <<~TESTCASE
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+TESTCASE
+
+def part1(contents)
+  lines = contents.split("\n")
+  pairs = lines.map { |line| line.split(/\s+/).map(&:to_i) }.transpose
+  pairs.map(&:sort!).first.each_with_index.reduce(0) do |sum, (num, i)|
+    sum + (pairs[0][i] - pairs[1][i]).abs
+  end
+end
+
+def part2(contents)
+  lines = contents.split("\n")
+  pairs = lines.map { |line| line.split(/\s+/).map(&:to_i) }.transpose
+  right = pairs[1].tally
+  pairs[0].reduce(0) do |sum, num|
+    sum + num * (right[num] || 0)
+  end
+end
+
+raise "Test failed! 11 = #{part1(TEST)}" unless part1(TEST) == 11
+puts part1(File.read('day1.txt'))
+
+raise "Test failed! 31 = #{part1(TEST)}" unless part2(TEST) == 31
+puts part2(File.read('day1.txt'))
---
+++ day2.rb
@@ -0,0 +1,48 @@
+TEST = <<~TESTCASE
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+TESTCASE
+
+def part1(contents)
+  lines = contents.split("\n")
+  parts = lines.map { |line| line.split(/\s+/).map(&:to_i) }
+  calculate(parts)
+end
+
+def valid?(part)
+  deltas = (0..part.length-2).map { |i| part[i] - part[i+1] }
+  deltas.all? { |delta| deltas[0].positive? == delta.positive? || deltas[0].negative? == delta.negative? } &&
+  deltas.all? { |delta| (1..3).include?(delta.abs) }
+end
+
+def calculate(parts)
+  parts.filter do |part|
+    valid?(part)
+  end.count
+end
+
+def part2(contents)
+  lines = contents.split("\n")
+  parts = lines.map { |line| line.split(/\s+/).map(&:to_i) }
+
+  parts.map do |part|
+    if valid?(part)
+      true
+    else
+      part.each_with_index.any? do |_, i|
+        valid?(part[0...i] + part[i+1..-1])
+      end
+    end
+  end.tally[true]
+
+end
+
+raise "Test failed! 2 = #{part1(TEST)}" unless part1(TEST) == 2
+puts part1(File.read('day2.txt'))
+
+raise "Test failed! 4 = #{part2(TEST)}" unless part2(TEST) == 4
+puts part2(File.read('day2.txt'))
